fc0f2ebbc49d6ba260bc35b06db1725af5649431
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-07-23 14:07:53 +0430

    Add basic Plugin class (no functionality yet)

---
+++ src/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MAChitgarha\LimeSurveyRestApi;
+
+use PluginBase;
+
+class Plugin extends PluginBase
+{
+    static protected $name = "LimeSurveyRestApi";
+    static protected $description = "LimeSurvey REST API provider";
+
+    public function init()
+    {
+    }
+}
