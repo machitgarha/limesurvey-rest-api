dda287227f9204a385a38852f985ca90d4191a5e
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-08-28 17:17:05 +0430

    Add some of useful survey properties for the endpoints

--- src/Api/Version0/SurveyController.php
+++ src/Api/Version0/SurveyController.php
@@ -3,6 +3,7 @@
 namespace MAChitgarha\LimeSurveyRestApi\Api\Version0;
 
 use Survey;
+use IApplicationComponent;
 
 use MAChitgarha\LimeSurveyRestApi\Api\Interfaces\Controller;
 
@@ -11,6 +12,7 @@ use MAChitgarha\LimeSurveyRestApi\Api\Traits;
 use MAChitgarha\LimeSurveyRestApi\Error\NotImplementedError;
 
 use MAChitgarha\LimeSurveyRestApi\Helper\Permission;
+use MAChitgarha\LimeSurveyRestApi\Helper\SurveyHelper;
 use MAChitgarha\LimeSurveyRestApi\Helper\PermissionChecker;
 
 use MAChitgarha\LimeSurveyRestApi\Utility\Response\JsonResponse;
@@ -35,14 +37,19 @@ class SurveyController implements Controller
 
         $userId = $this->authorize()->getId();
 
-        $survey = Survey::model();
-        $survey->permission($userId);
+        PermissionChecker::assertHasSurveyPermission(
+            $survey = Survey::model(),
+            Permission::READ,
+            $userId
+        );
 
-        $userSurveys = $survey->findAll();
+        $surveyList = $survey->findAll();
 
         $data = [];
-        foreach ($userSurveys as $survey) {
-            $data[] = self::makeSurveyData($survey);
+        foreach ($surveyList as $survey) {
+            $data[] = self::makeSurveyDataFromInfo(
+                SurveyHelper::getInfo($survey->sid)
+            );
         }
 
         return new JsonResponse(
@@ -56,29 +63,50 @@ class SurveyController implements Controller
 
         $userId = $this->authorize()->getId();
 
-        $survey = Survey::model()->findByPk(
+        $surveyInfo = SurveyHelper::getInfo(
             $this->getPathParameterAsInt('survey_id')
         );
 
         PermissionChecker::assertHasSurveyPermission(
-            $survey,
+            $surveyInfo['oSurvey'],
             Permission::READ,
             $userId
         );
 
         return new JsonResponse(
-            data(self::makeSurveyData($survey))
+            data(self::makeSurveyDataFromInfo($surveyInfo))
         );
     }
 
-    private static function makeSurveyData(Survey $survey): array
+    private static function makeSurveyDataFromInfo(array $surveyInfo): array
     {
         return [
-            'id' => $survey->sid,
-            'is_active' => $survey->active !== 'N',
-            'creation_time' => $survey->datecreated,
+            'id' => $surveyInfo['sid'],
+            'group_id' => $surveyInfo['gsid'],
+
+            'creation_time' => $surveyInfo['datecreated'],
+            'start_time' => $surveyInfo['startdate'],
+            'expiry_time' => $surveyInfo['expires'],
+
+            'format' => $surveyInfo['format'],
+
+            'is_active' => $surveyInfo['active'] === 'Y',
+            'is_backward_navigation_allowed' => $surveyInfo['allowprev'] === 'Y',
+            'is_datestamps_stored' => $surveyInfo['datestamp'] === 'Y',
+            'is_ip_address_stored' => $surveyInfo['ipaddr'] === 'Y',
+            'is_progress_shown' => $surveyInfo['showprogress'] === 'Y',
+            'is_welcome_message_shown' => $surveyInfo['showwelcome'] === 'Y',
+
+            'navigation_delay' => $surveyInfo['navigationdelay'],
+
             'l10n' => [
-                'title' => $survey->languagesettings[$survey->language]->surveyls_title ?? ''
+                'title' => $surveyInfo['surveyls_title'],
+                'description' => $surveyInfo['surveyls_description'],
+
+                'welcome_message' => $surveyInfo['surveyls_welcometext'],
+                'end_message' => $surveyInfo['surveyls_endtext'],
+                'end_url' => $surveyInfo['surveyls_url'],
+                'end_url_title' => $surveyInfo['surveyls_urldescription'],
             ],
         ];
     }
