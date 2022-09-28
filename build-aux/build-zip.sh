b107cee391fb7bcb59444e435d07abe9cc3febaa
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2022-09-28 07:13:04 +0330

    Add build-aux/build-zip.sh for building ZIP file
    Ignore the resulting file in .gitignore.

---
+++ build-aux/build-zip.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Config
+pluginName="RestApi"
+outputFilename="limesurvey-rest-api.zip"
+
+# Exit on error
+set -e
+
+mainRepoDir="$(dirname "$(realpath "$0/..")")"
+# The resulting file path
+zipFilePath="$mainRepoDir/$outputFilename"
+
+cd "$mainRepoDir"
+
+# Composer dev dependencies are huge.
+echo "Removing Composer dev dependencies..."
+composer --no-dev install > /dev/null 2>&1
+
+# Remove previous zip file to prevent extra removed files to remain there
+if [[ -e "$zipFilePath" ]]; then
+    rm "$zipFilePath"
+fi
+
+# Create the zip file in the current directory
+zip -r -y "$zipFilePath" "spec/" "src/" "vendor/" "config.xml" "LICENSE.md" "$pluginName.php" > /dev/null
+
+echo "Zip file created successfully."
+
+echo "Re-adding Composer dev dependencies..."
+composer install > /dev/null 2>&1
